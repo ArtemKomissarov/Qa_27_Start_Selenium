dee9d2eb18df24c355dbb57ed363d9c0886d4b58
ArtemKomissarov <166131488+ArtemKomissarov@users.noreply.github.com>
2024-10-24 17:36:38 +0500

    Home Work1

--- src/test/java/PhoneBook.java
+++ src/test/java/PhoneBook.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -18,13 +20,44 @@ public class PhoneBook {
     @Test
     public void selectors(){
         //by tagName
+        WebElement  body = wd.findElement(By.tagName("body"));
+        WebElement body1 = wd.findElement(By.cssSelector("body"));
+
+        WebElement div = wd.findElement(By.tagName("div"));
+        WebElement div1 = wd.findElement(By.cssSelector("div"));
+
+        WebElement h1 = wd.findElement(By.tagName("h1"));
+        WebElement h1_1 = wd.findElement(By.cssSelector("h1"));
+
+
+        WebElement a = wd.findElement(By.tagName("a"));
+        WebElement a1 = wd.findElement(By.cssSelector("a"));
+
+
+        WebElement input = wd.findElement(By.tagName("input"));
+        WebElement input1 = wd.findElement(By.cssSelector("input"));
+
+        WebElement button = wd.findElement(By.tagName("button"));
+        WebElement button1 = wd.findElement(By.cssSelector("button"));
 
 
 
         //by className
+        WebElement el = wd.findElement(By.className("container"));
+        WebElement el1 = wd.findElement(By.cssSelector("container"));
+
+        WebElement el2 = wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        WebElement el3 = wd.findElement(By.cssSelector(".navbar-component_nav__1X_4m"));
+
+        WebElement el4 = wd.findElement(By.cssSelector(".active"));
+        WebElement el5 = wd.findElement(By.className("active"));
+
 
 
         //by id
+
+        WebElement el6 = wd.findElement(By.id("root"));
+        WebElement el7 = wd.findElement(By.cssSelector("#root"));
 
 
 
